33186701fba1db3f67b8e0154dd539ff431c8371
Brian Maxwell <brianmaxwell1@gmail.com>
2016-02-11 19:53:39 -0500

    added skeleton activities and built simple menu to navigate between them

---
+++ app/src/main/java/com/nineinchmales/longcircuit/GameplayActivity.java
@@ -0,0 +1,13 @@
+package com.nineinchmales.longcircuit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GameplayActivity extends ActivityWithMenu {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gameplay);
+    }
+}
---
+++ app/src/main/java/com/nineinchmales/longcircuit/HighScoresActivity.java
@@ -0,0 +1,13 @@
+package com.nineinchmales.longcircuit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HighScoresActivity extends ActivityWithMenu {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_high_scores);
+    }
+}

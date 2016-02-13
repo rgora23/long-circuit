c7ad861c6d7fb87de32675c6dddd4751b70c735d
Harrison Huynh <harrison,huynh17@gmail.com>
2016-02-13 13:00:43 -0500

    --message

--- app/src/main/java/com/nineinchmales/longcircuit/activities/MainMenuActivity.java
+++ app/src/main/java/com/nineinchmales/longcircuit/activities/MainMenuActivity.java
@@ -1,6 +1,9 @@
 package com.nineinchmales.longcircuit.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 import com.nineinchmales.longcircuit.R;
 import com.nineinchmales.longcircuit.LongCircuitActivity;
@@ -17,8 +20,19 @@ public class MainMenuActivity extends LongCircuitActivity {
         setContentView(R.layout.activity_main_menu);
         setTitle("Main Menu");
         presenter = new MainMenuPresenter(this);
-    }
 
+        initPlayButton();
+    }
+    private void initPlayButton() {
+        ImageButton list = (ImageButton) findViewById(R.id.PlayButton);
+        list.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(MainMenuActivity.this, GameplayActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+            }
+        });
+    }
     @Override
     public MainMenuPresenter getPresenter() {
         return presenter;
